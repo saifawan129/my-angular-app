4b0e89b9ba1b5ecdf7192f9fbaca20e322d24fb8
saifawan129 <sa949349@gmail.com>
2025-07-21 18:39:51 +0500

    created component using Modern for if in angular

--- src/app/appss/app.component.ts
+++ src/app/appss/app.component.ts
@@ -3,11 +3,13 @@ import { HeaderComponent } from '../file/file.component';
 import { DUMMY_USERS } from '../dummy-user';
 import { UserComponent } from '../user/user.component';
 import { TasksComponent } from '../tasks/tasks.component';
+import { NgFor, NgIf } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
@@ -17,7 +19,7 @@ export class AppComponent {
   title = 'my-angular-app';
   users = DUMMY_USERS;
   // DUMMY_USERS is an array of user objects imported from dummy-user.ts
-  SelectUserId = 'u1';
+  SelectUserId?: string ;
   // This is the id of the user that is selected by default
 
   get selectedUser() {
@@ -30,3 +32,4 @@ export class AppComponent {
     //set the SelectUserId to the id of the user that is selected
   }
 }
+
